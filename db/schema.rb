617bbaf9b6f1c38d5b121b4229efc5595718f481
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-11-10 21:06:24 -0500

    added twitter image to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111001057) do
+ActiveRecord::Schema.define(:version => 20131111020214) do
 
   create_table "messages", :force => true do |t|
     t.text     "message"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20131111001057) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "image"
   end
 
 end
