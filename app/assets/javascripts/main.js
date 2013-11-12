f3ac9b9ea9d6fd45b5f519abb2b4705b743ff306
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-11-12 14:41:43 -0500

    added start menu

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -83,6 +83,12 @@ Physijs.scripts.ammo = 'ammo.js';
 
 var blocker = document.getElementById( 'blocker' );
 var instructions = document.getElementById( 'instructions' );
+var startMenu = document.getElementById( 'start-menu' );
+var startButton = document.getElementById( 'start-button' );
+instructions.style.display = 'none';
+startMenu.style.display = '';
+
+
 
       // POINTERLOCK CODE - FROM http://www.html5rocks.com/en/tutorials/pointerlock/intro/
       var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
@@ -128,9 +134,47 @@ var instructions = document.getElementById( 'instructions' );
         document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
         document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
 
+
+
         instructions.addEventListener( 'click', function ( event ) {
 
           instructions.style.display = 'none';
+
+          // Ask the browser to lock the pointer
+          element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+          if ( /Firefox/i.test( navigator.userAgent ) ) {
+
+            var fullscreenchange = function ( event ) {
+
+              if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+
+                document.removeEventListener( 'fullscreenchange', fullscreenchange );
+                document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+
+                element.requestPointerLock();
+              }
+
+            };
+
+            document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+            document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+            element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+            element.requestFullscreen();
+
+          } else {
+
+            element.requestPointerLock();
+
+          }
+
+        }, false );
+
+        startButton.addEventListener( 'click', function ( event ) {
+
+          startMenu.style.display = 'none';
 
           // Ask the browser to lock the pointer
           element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
@@ -169,6 +213,12 @@ var instructions = document.getElementById( 'instructions' );
   instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 
 }
+
+
+
+
+
+
 // LOAD AUDIO
 loadAudioRequest( url );
 
